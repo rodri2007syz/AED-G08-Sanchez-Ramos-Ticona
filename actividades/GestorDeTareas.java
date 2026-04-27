b6eec17eee11c0efbde4f0a05c21da2b5e4621b5
R0drigo23 <rodrigo.ramos@estudiante.ucsm.edu.pe>
2026-04-26 20:21:06 -0500

    Add files via upload
    "Actividad: gestor de tareas implementado"

--- actividades/GestorDeTareas.java
+++ actividades/GestorDeTareas.java
@@ -1,5 +1,36 @@
 package actividades;
 
-public class GestorDeTareas {
+public class GestorDeTareas<T extends Comparable<T>> {
+
+    private ListLinked<T> lista;
+
+    public GestorDeTareas(){
+        lista=
+          new ListLinked<T>();
+    }
+
+    public void agregarTarea(T t){
+        lista.insertLast(t);
+    }
+
+    public boolean eliminarTarea(T t){
+        return lista.removeNode(t);
+    }
+
+    public boolean contieneTarea(T t){
+        return lista.search(t);
+    }
+
+    public void imprimirTareas(){
+        lista.print();
+    }
+
+    public int contarTareas(){
+        return lista.length();
+    }
+
+    public void invertirTareas(){
+        lista.reverse();
+    }
 
 }
