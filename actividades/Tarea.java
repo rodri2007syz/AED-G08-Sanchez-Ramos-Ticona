a403fe6267f4cf794c300aa04bace722b097f595
R0drigo23 <rodrigo.ramos@estudiante.ucsm.edu.pe>
2026-04-26 20:18:42 -0500

    Add files via upload
    "Actividad: clase tarea"

--- actividades/Tarea.java
+++ actividades/Tarea.java
@@ -1,5 +1,41 @@
 package actividades;
 
-public class Tarea {
+public class Tarea implements Comparable<Tarea>{
+
+    private String titulo;
+    private int prioridad;
+    private String estado;
+
+    public Tarea(String titulo,int prioridad,String estado){
+        this.titulo=titulo;
+        this.prioridad=prioridad;
+        this.estado=estado;
+    }
+
+    public String getTitulo(){
+        return titulo;
+    }
+
+    public int getPrioridad(){
+        return prioridad;
+    }
+
+    public String getEstado(){
+        return estado;
+    }
+
+    public void setEstado(String estado){
+        this.estado=estado;
+    }
+
+    public int compareTo(Tarea otra){
+        return prioridad-otra.prioridad;
+    }
+
+    public String toString(){
+        return "["+titulo+
+                " | Prioridad: "+prioridad+
+                " | Estado: "+estado+"]";
+    }
 
 }
