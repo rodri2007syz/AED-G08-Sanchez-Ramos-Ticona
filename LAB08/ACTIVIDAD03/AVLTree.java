639daf218643b27e3bdb69bb88493707c96af9d7
Sebastian-Ticona <Sebastian.ticona@estudiante.ucsm.edu.pe>
2026-05-17 18:51:55 -0500

    ACTIVIDAD 03 hice la insercion del árbol AVL agregando actualización del factor de equilibrio y detección de desbalances

--- LAB08/ACTIVIDAD03/AVLTree.java
+++ LAB08/ACTIVIDAD03/AVLTree.java
@@ -10,4 +10,77 @@ public class AVLTree extends BSTree {
 
         height = false;
     }
+    public void insert(int data) {
+
+        root = insertAVL((NodeAVL) root, data);
+    }
+
+    private NodeAVL insertAVL(NodeAVL node, int data) {
+
+        // inserto nuevo nodo
+        if (node == null) {
+
+            height = true;
+
+            return new NodeAVL(data);
+        }
+
+        // inserto por izquierda
+        if (data < node.data) {
+
+            node.left = insertAVL((NodeAVL) node.left, data);
+
+            if (height) {
+
+                node.bf--;
+
+                // reviso desbalance
+                if (node.bf == -2) {
+
+                    node = balanceToRight(node);
+
+                    height = false;
+                }
+
+                else if (node.bf == 0) {
+                    height = false;
+                }
+            }
+        }
+
+        // inserto por derecha
+        else if (data > node.data) {
+
+            node.right = insertAVL((NodeAVL) node.right, data);
+
+            if (height) {
+
+                node.bf++;
+
+                // reviso desbalance
+                if (node.bf == 2) {
+
+                    node = balanceToLeft(node);
+
+                    height = false;
+                }
+
+                else if (node.bf == 0) {
+                    height = false;
+                }
+            }
+        }
+
+        return node;
+    }
+
+    private NodeAVL balanceToLeft(NodeAVL node) {
+
+        return node;
+    }
+
+    private NodeAVL balanceToRight(NodeAVL node) {
+
+        return node;
+    }
 }
